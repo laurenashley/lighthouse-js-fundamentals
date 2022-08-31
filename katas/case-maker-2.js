baeeaf3b49e4f3974d2dfa33a0447f6026e6d904
Lauren Ashley <la.zaubi@gmail.com>
2022-08-30 21:43:57 -0700

    change string to many different cases

--- katas/case-maker-2.js
+++ katas/case-maker-2.js
@@ -1,12 +1,8 @@
 const makeCase = function (input, c) {
-  let noSpaces = input.replace(/[^A-Z0-9]/ig, ' ').split(' ');
   let result = [];
 
-  // remove special characters/punctuation
-  // let string = input.replace(/[^A-Z0-9]/ig, ' ');
-  // console.log(string);
-
-  const makeCamelCase = function(noSpaces) {
+  const makeCamelCase = function(input) {
+    let noSpaces = input.replace(/[^A-Z0-9]/ig, ' ').split(' ');
     for (let i = 0; i < noSpaces.length; i++) {
       result.push( i === 0 ?  noSpaces[i] : noSpaces[i].charAt(0).toUpperCase() + noSpaces[i].slice(1));
     }
@@ -21,32 +17,52 @@ const makeCase = function (input, c) {
     result = result.join(' ');
   };
 
-  switch (c) {
-    case 'camel':
-      makeCamelCase(noSpaces);
-    break;
-    case 'pascal':
-      makeCamelCase(noSpaces);
-    break;
-    case 'snake':
-      result = input.replace(/[^A-Z0-9]/ig, '_');
-    break;
-    case 'kebab':
-      result = input.replace(/[^A-Z0-9]/ig, '-');
-    break;
-    case 'title':
-      makeTitleCase(input);
-    break;
-    case 'vowel':
-      result = input.replace((/[aeiou]/g, function(e) {
-        console.log(e);
-        return e.toUpperCase();
-      }));
-    break;
+  const caseSwitch = function(c) {
+    switch (c) {
+      case 'camel':
+        makeCamelCase(input);
+      break;
+      case 'pascal':
+        makeCamelCase(input);
+      break;
+      case 'snake':
+        console.log(c, 'result is ', result);
+        result = result !== [] ? result[0].replace(/[^A-Z0-9]/ig, '_') : input.replace(/[^A-Z0-9]/ig, '_');
+      break;
+      case 'kebab':
+        result = input.replace(/[^A-Z0-9]/gi, '-');
+      break;
+      case 'title':
+        makeTitleCase(input);
+      break;
+      case 'vowel':
+        result = input.replace(/[aeiou]/gi, (e) => { 
+          return e.toUpperCase();
+      });
+      break;
+      case 'consonant':
+        result = input.replace(/[bcdfghjklmnpqrstvwxys]/gi, (e) => { 
+          return e.toUpperCase();
+      });
+      break;
+      case 'upper':
+        console.log('result is ', result, input);
+        result = result === [] ? input.toUpperCase() : result[0].toUpperCase();
+        console.log(result);
+      break;
+    }
   }
+
+  const caseIsArray = Array.isArray(c);
+  const caseArray = caseIsArray ? c : [];
+
+  caseIsArray ? caseArray.forEach(e => caseSwitch(e)) : caseSwitch(c);
 
   return result;
 }
+
+
+console.log(makeCase("this is a string", ["snake", "upper"]));
 
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
@@ -54,5 +70,5 @@ console.log(makeCase("this is a string", "snake"));
 console.log(makeCase("this is a string", "kebab"));
 console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
-// console.log(makeCase("this is a string", "consonant"));
-// console.log(makeCase("this is a string", ["upper", "snake"]));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
